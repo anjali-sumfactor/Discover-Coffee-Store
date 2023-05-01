c4669e1b7fd3a25292da24d6db9fb922815a74d1
anjali-sumfactor <anjali.sumfactor@gmail.com>
2023-05-01 16:54:45 +0530

    coffee stores

---
+++ components/banner.js
@@ -0,0 +1,14 @@
+import styles from "./banner.module.css";
+
+export function Banner(props) {
+    return <>
+        <div className={styles.container}>
+            <h1 className={styles.title}>
+                <span className={styles.title1}>Coffee</span>
+                <spna className={styles.title2}>Connoisseur</spna>
+            </h1>
+            <p className={styles.subTitle}>Discover your local coffee shops!</p>
+            <button className={styles.button} onClick={props.handleOnClick}>{props.buttonText}</button>
+        </div>
+    </>
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
 import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
+import { Banner } from '@/components/banner'
 
 export default function Home() {
+  const handleOnBannerBtnClick=()=>{
+    console.log("hi banner button")
+  }
+  
   return (
     <>
       <Head>
@@ -11,7 +16,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <h1>Coffee Connoisseur</h1>
+        <Banner buttonText="View stores nearby" handleOnClick={handleOnBannerBtnClick}/>
       </main>
     </>
   )
