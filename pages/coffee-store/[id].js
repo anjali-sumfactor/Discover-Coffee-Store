7de6ced6137ee35f56e89694a185af38c523a6de
anjali-sumfactor <anjali.sumfactor@gmail.com>
2023-05-05 16:26:26 +0530

    coffee stores

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,9 +1,36 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
 
-export default function CoffeeStore() {
+import coffeeStoresData from '../../data/coffee-stores.json';
+
+export function getStaticProps(staticProps) {
+    const params = staticProps.params;
+    console.log('params', params);
+
+    return {
+        props: {
+            coffeeStore: coffeeStoresData.find(coffeeStore => {
+                return coffeeStore.id.toString() === params.id; //dynamic id
+            })
+        }
+    }
+}
+
+export function getStaticPaths() {
+    return {
+        paths: [
+            { params: { id: '0' } },
+            { params: { id: '1' } }
+        ],
+        fallback: true,
+    }
+}
+
+export default function CoffeeStore(props) {
     const router = useRouter();
     console.log("router", router);
+
+    console.log('props', props);
 
     return (
         <>
@@ -11,6 +38,8 @@ export default function CoffeeStore() {
                 coffee store page {router.query.id}
                 <Link href="/">Back to home</Link>
                 <Link href="/coffee-store/dynamic">Go to page dynamic</Link>
+                <p>{props.coffeeStore.address}</p>
+                <p>{props.coffeeStore.name}</p>
             </div>
         </>
     );
